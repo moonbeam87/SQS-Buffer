0d85217676fd526e0cd9bd7d14d98ebe83ed43ae
moonbeam87 <2021dpatel@gmail.com>
2020-08-09 10:24:53 -0400

    fixed timing issues?

--- receive.py
+++ receive.py
@@ -1,62 +1,70 @@
 import boto3
 import json
 import pandas as pd
-sqs = boto3.client('sqs')
 
-queue_url = 'https://sqs.us-east-1.amazonaws.com/306784070391/test'
 
-#Receive SQS Message
-response = sqs.receive_message(
-    QueueUrl=queue_url,
-    AttributeNames=[
-        'SentTimestamp'
-    ],
-    MaxNumberOfMessages=1,
-    MessageAttributeNames=[
-        'All'
-    ],
-    VisibilityTimeout=0,
-    WaitTimeSeconds=0
-)
+def receiveMessage():
+    sqs = boto3.client('sqs')
 
-message = response['Messages'][0]
+    queue_url = 'https://sqs.us-east-1.amazonaws.com/306784070391/test'
 
-receipt_handle = message['ReceiptHandle']
-message = json.dumps(message)
-# Delete received message from queue
-sqs.delete_message(
-    QueueUrl=queue_url,
-    ReceiptHandle=receipt_handle
-)
+    #Receive SQS Message
+    response = sqs.receive_message(
+        QueueUrl=queue_url,
+        AttributeNames=[
+            'SentTimestamp'
+        ],
+        MaxNumberOfMessages=1,
+        MessageAttributeNames=[
+            'All'
+        ],
+        VisibilityTimeout=0,
+        WaitTimeSeconds=0
+    )
 
-#print('Received and deleted message: %s' % message)
+    message = response['Messages'][0]
 
-column1 = 'MessageAttributes__|'
-column2 = 'MessageAttributes__|__StringValue'
-df = pd.read_json(message)
-#print("-------------------")
-#print(df)
-#print("--------------------")
-author = df['MessageAttributes']['Author']
-author = author['StringValue']
-title = df['MessageAttributes']['Title']
-title = title['StringValue']
-weeks = df['MessageAttributes']['WeeksOn']
-weeks = weeks['StringValue']
-#print("-------------------")
-#print(author)
-#print(title)
-#print(weeks)
-#print("--------------------")
-dynamodb = boto3.resource('dynamodb')
+    receipt_handle = message['ReceiptHandle']
+    message = json.dumps(message)
+    # Delete received message from queue
+    sqs.delete_message(
+        QueueUrl=queue_url,
+        ReceiptHandle=receipt_handle
+    )
 
-table = dynamodb.Table('test')
+    #print('Received and deleted message: %s' % message)
 
-table.put_item(
-   Item={
-        'title':title,
-        'author':author,
-        'weeks':weeks,
-        'SQSBookAttributes':message,
-    }
-)
+    column1 = 'MessageAttributes__|'
+    column2 = 'MessageAttributes__|__StringValue'
+    df = pd.read_json(message)
+    #print("-------------------")
+    #print(df)
+    #print("--------------------")
+    author = df['MessageAttributes']['Author']
+    author = author['StringValue']
+    title = df['MessageAttributes']['Title']
+    title = title['StringValue']
+    weeks = df['MessageAttributes']['WeeksOn']
+    weeks = weeks['StringValue']
+    #print("-------------------")
+    #print(author)
+    #print(title)
+    #print(weeks)
+    #print("--------------------")
+    dynamodb = boto3.resource('dynamodb')
+
+    table = dynamodb.Table('test')
+
+    table.put_item(
+    Item={
+            'title':title,
+            'author':author,
+            'weeks':weeks,
+            'SQSBookAttributes':message,
+        }
+    )
+
+import time
+while True:
+    receiveMessage()
+    time.sleep(15)
